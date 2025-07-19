c3e6ba4a298dd447eb4db8252adcb11a71350270
Shashi007kumar <shashikumar29111987@gmail.com>
2025-07-19 12:59:57 +0530

    cloudinary setup complete

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+
+// cd => callback
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.originalname + '-' + uniqueSuffix)
+  }
+})
+
+export const upload = multer({ storage: storage })
+// in ES6 we can write storage only beacuse both has same name
